54642e73f656ae6dca270e66032f08f465b786c0
mr.debbie <giyoungchong1980@gmail.com>
2017-01-30 15:16:13 -0600

    Add methods

--- Promo.java
+++ Promo.java
@@ -14,9 +14,115 @@ public class Promo {
   double originalPrice, promo, salePrice, discount, totalSavings = 0;
   final double perCheck = .1;
   final int check = 0;
+  //choice = fileChoice();
+  choice = fileChoice();
+  if (choice == 1){
+    creatNewFile();
+    username = getUsername();
+    day = getDay();
+    itemname = getItem();
 
+  }
+  else{
+    openFile();
+    username = getUsername();
+    day = getDay();
+    itemname = getItem();
+
+  }
+  }//close main
+  public static int fileChoice(){
+  int input;
   //create a Scanner object to read input
   Scanner keyboard = new Scanner (System.in);
+  System.out.print("Would you like to (1) create a new file or (2) add to an existing file?");
+  input = keyboard.nextInt();
+  keyboard.nextLine();
+  return input;
+  }
+  public static void creatNewFile() throws IOException{
+  String fName;
+  Scanner keyboard = new Scanner (System.in);
+  System.out.print("Enter the filename: ");
+  fName = keyboard.nextLine();
+  File file = new File (fName);
+  while (file.exists()){
+    System.out.println("The file " + fName + " already exists");
+    System.out.print("Enter a new filename: ");
+    fName = keyboard.nextLine();
+  }
+  }
+  public static String openFile () throws IOException{
+  String fName;
+  Scanner keyboard = new Scanner (System.in);
+  System.out.print("Enter the filename: ");
+  fName = keyboard.nextLine();
+  FileWriter fwriter = new FileWriter(fName, true);
+  PrintWriter outputFile = new PrintWriter(fwriter);
+  return fName;
+  }
+  public static String getUsername (){
+  String uName;
+  Scanner keyboard = new Scanner (System.in);
+  System.out.print("Enter your username: ");
+  uName = keyboard.nextLine();
+  return uName;
+  }
+  public static String getDay (){
+  String userDay;
+  boolean check = false;
+  Scanner keyboard = new Scanner (System.in);
+  System.out.print("Enter the day: ");
+  userDay = keyboard.nextLine();
+  switch (userDay){
+    case "Monday":
+    case "monday":
+    check = true;
+    break;
+    case "Tuesday":
+    case "tuesday":
+    check = true;
+    break;
+    case "Wednesday":
+    case "wednesday":
+    check = true;
+    break;
+    case "Thursday":
+    case "thursday":
+    check = true;
+    break;
+    case "Friday":
+    case "friday":
+    check = true;
+    break;
+    case "Saturday":
+    case "saturday":
+    check = true;
+    break;
+    case "Sunday":
+    case "sunday":
+    check = true;
+    break;
+    default:
+    check = false;
+    }
+    while (!check){
+      System.out.print("Invalid input. Enter day of the week again: ");
+      userDay = keyboard.nextLine();
+      check = true;
+  }
+  //need to reset flag, ouuuuuuuu???????
+  return userDay;
+  }
+  public static String getItem (){
+  String userItem;
+  Scanner keyboard = new Scanner (System.in);
+  System.out.print("Enter the item name: ");
+  userItem = keyboard.nextLine();
+  return userItem;
+  }
+  //create a Scanner object to read input
+  /*Scanner keyboard = new Scanner (System.in);
   do {
     //Get filename
     System.out.print("Would you like to (1) create a new file or (2) add to an existing file?");
@@ -121,7 +227,7 @@ public class Promo {
         /*if (totalSavings <= discount){
         System.out.println("Way to go!");
       }*/
-      System.out.println("Would you like to enter another purchase?");
+      /*System.out.println("Would you like to enter another purchase?");
       keyboard.nextLine();
       System.out.print("Enter (Y)yes or (N)no: ");
       repeat = keyboard.nextLine();
@@ -227,7 +333,7 @@ else{
     /*if (totalSavings <= discount){
     System.out.println("Way to go!");
   }*/
-  System.out.println("Would you like to enter another purchase?");
+  /*System.out.println("Would you like to enter another purchase?");
   keyboard.nextLine();
   System.out.print("Enter (Y)yes or (N)no: ");
   repeat = keyboard.nextLine();
@@ -339,9 +445,9 @@ else {
   openSesame = false;
 }*/
 
-}while (openSesame);
+/*}while (openSesame);
 System.out.println("Thanks for using Yang's Money Tracker!");
 System.out.println("$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$");
 
-}
+}*/
 }
