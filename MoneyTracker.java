26f6c99704d2749dfa6d9dfd1474e22d2e2283ee
mr.debbie <giyoungchong1980@gmail.com>
2017-02-02 19:23:25 -0600

    Test method calls in main

--- MoneyTracker.java
+++ MoneyTracker.java
@@ -7,27 +7,41 @@ public class MoneyTracker {
   //declare variables
   String userName, password, filename;
   int choice;
- Scanner keyboard = new Scanner(System.in);
- System.out.print("Enter your user name: ");
- userName = keyboard.nextLine();
- System.out.print("Enter your password: ");
- password = keyboard.nextLine();
- System.out.print("Would you like to add data to (1) a new file or (2) an existing file ? ");
- choice = keyboard.nextInt();
- keyboard.nextLine();
- if (choice == 1){
-  System.out.print("Enter the new filename: ");
-  filename = keyboard.nextLine();
-}
-else {
-System.out.print("Enter the existing filename: ");
-filename = keyboard.nextLine();
-}
 
+  filename = createNewFile();
+  userName = getUsername();
+  password = getPassword();
+
+//create user
 User user1 = new User (userName, password, filename);
 System.out.println(user1.getName());
 System.out.println(user1.getPassword());
 System.out.println(user1.getFilename());
+}//close main method
 
+  public static String createNewFile() throws IOException{
+  String fName;
+  Scanner keyboard = new Scanner (System.in);
+  System.out.print("Enter the filename: ");
+  fName = keyboard.nextLine();
+  File file = new File (fName);
+  while (file.exists()){
+    System.out.println("The file " + fName + " already exists");
+    System.out.print("Enter a new filename: ");
+    fName = keyboard.nextLine();
+  }
+  public static String getUsername (){
+  String uName;
+  Scanner keyboard = new Scanner (System.in);
+  System.out.print("Enter your username: ");
+  uName = keyboard.nextLine();
+  return uName;
+  }
+  public static String getPassword (){
+  String pWord;
+  Scanner keyboard = new Scanner (System.in);
+  System.out.print("Enter your password: ");
+  pWord = keyboard.nextLine();
+  return pWord;
   }
 }
