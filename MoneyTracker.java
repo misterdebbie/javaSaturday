17342f1d48541200b47515b401b9088decf43220
mr.debbie <giyoungchong1980@gmail.com>
2017-02-02 22:19:48 -0600

    Update main method

--- MoneyTracker.java
+++ MoneyTracker.java
@@ -5,14 +5,16 @@ import java.io.*;
 public class MoneyTracker {
  public static void main (String[] args)throws IOException{
   //declare variables
-  String userName, password, filename, itemname;
+  String userName, password, filename, itemname, day;
   int choice;
-  double originalPrice, discount, finalPrice;
+  double originalPrice, discount, finalPrice, totalDisco;
+  boolean dayCheck = false;
 
-  //ProfileSetup user1 = new ProfileSetup();
-  filename = createNewFile();
+
   userName = setUsername();
   password = setPassword();
+  filename = createNewFile();
+  day = getDay();
   //create User
   User user1 = new User (userName, password, filename);
   itemname = setItem();
@@ -21,10 +23,13 @@ public class MoneyTracker {
   //create Item
   Item item1 = new Item(itemname, originalPrice, discount);
   finalPrice = item1.getFinalCost();
-System.out.println(user1.getName());
+  totalDisco = item1.getTotalDiscount();
+  writeToNewFile(user1, day, item1);
+
+/*System.out.println(user1.getName());
 System.out.println(user1.getPassword());
 System.out.println(user1.getFilename());
-System.out.println("Final cost of " + item1.getName() + ": $"+  finalPrice);
+System.out.println("Final cost of " + item1.getName() + ": $"+  finalPrice);*/
 }//close main method
 
   public static String createNewFile() throws IOException{
@@ -53,6 +58,52 @@ System.out.println("Final cost of " + item1.getName() + ": $"+  finalPrice);
   System.out.print("Enter your password: ");
   pWord = keyboard.nextLine();
   return pWord;
+}
+public static String getDay (){
+String userDay;
+boolean check = false;
+Scanner keyboard = new Scanner (System.in);
+System.out.print("Enter the day: ");
+userDay = keyboard.nextLine();
+switch (userDay){
+  case "Monday":
+  case "monday":
+  check = true;
+  break;
+  case "Tuesday":
+  case "tuesday":
+  check = true;
+  break;
+  case "Wednesday":
+  case "wednesday":
+  check = true;
+  break;
+  case "Thursday":
+  case "thursday":
+  check = true;
+  break;
+  case "Friday":
+  case "friday":
+  check = true;
+  break;
+  case "Saturday":
+  case "saturday":
+  check = true;
+  break;
+  case "Sunday":
+  case "sunday":
+  check = true;
+  break;
+  default:
+  check = false;
+  }
+  while (!check){
+    System.out.print("Invalid input. Enter day of the week again: ");
+    userDay = keyboard.nextLine();
+    check = true;
+}
+//need to reset flag, ouuuuuuuu???????
+return userDay;
 }
 public static String setItem (){
 String userItem;
@@ -87,11 +138,24 @@ public static double setDiscount (){
 }
   return promo;
 }
-public static double getFinalPrice (double original, double promo){
-
-  double discount, finalCost;
-  discount = original * promo;
-  finalCost = original - discount;
-  return finalCost;
+/*public static void writeToNewFile(String fName, String uName, String userItem, String day, double originalCost, double disco, double finalCost) throws IOException{
+PrintWriter outputFile = new PrintWriter (fName);
+outputFile.printf("Username: %s, Day: %s, Item: %s\n", uName, day, userItem);
+outputFile.printf("You saved $%,.2f\n", disco);
+outputFile.printf("Your final cost is $%,.2f\n", finalCost);
+//outputFile.printf("Accumulated savings of $%,.2f\n", totalSavings);
+//close the file
+outputFile.close();
+System.out.println("Data written to the file.");
+}*/
+public static void writeToNewFile(User testUser, String day, Item testItem) throws IOException{
+PrintWriter outputFile = new PrintWriter (testUser.getFilename());
+outputFile.printf("Username: %s, Day: %s, Item: %s\n", testUser.getName(), day, testItem.getName());
+outputFile.printf("You saved $%,.2f\n", testItem.getTotalDiscount());
+outputFile.printf("Your final cost is $%,.2f\n", testItem.getFinalCost());
+//outputFile.printf("Accumulated savings of $%,.2f\n", totalSavings);
+//close the file
+outputFile.close();
+System.out.println("Data written to the file.");
 }
 }
