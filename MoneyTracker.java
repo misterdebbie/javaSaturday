07eefe2287862add7cd782aacd021f4b5889623a
mr.debbie <giyoungchong1980@gmail.com>
2017-02-02 22:54:36 -0600

    Wrap it up in a do while

--- MoneyTracker.java
+++ MoneyTracker.java
@@ -9,111 +9,149 @@ public class MoneyTracker {
   int choice;
   double originalPrice, discount, finalPrice, totalDisco;
   boolean dayCheck = false;
+  boolean openSesame = false;
 
+  do {
+  choice = fileChoice();
+  if (choice == 1){
+    userName = setUsername();
+    password = setPassword();
+    filename = createNewFile();
+    day = setDay();
+    //create User
+    User user1 = new User (userName, password, filename);
+    itemname = setItem();
+    originalPrice = setItemCost();
+    discount = setDiscount();
+    //create Item
+    Item item1 = new Item(itemname, originalPrice, discount);
+    finalPrice = item1.getFinalCost();
+    totalDisco = item1.getTotalDiscount();
+    writeToNewFile(user1, day, item1);
+    openSesame = addData();
+  }
+  else{
+    userName = setUsername();
+    password = setPassword();
+    filename = openFile();
+    day = setDay();
+    //create User
+    User user1 = new User (userName, password, filename);
+    itemname = setItem();
+    originalPrice = setItemCost();
+    discount = setDiscount();
+    //create Item
+    Item item1 = new Item(itemname, originalPrice, discount);
+    finalPrice = item1.getFinalCost();
+    totalDisco = item1.getTotalDiscount();
+    addToFile(user1, day, item1);
+    System.out.println(finalPrice);
+    openSesame = addData();
+  }
+  }while(openSesame);
 
-  userName = setUsername();
-  password = setPassword();
-  filename = createNewFile();
-  day = getDay();
-  //create User
-  User user1 = new User (userName, password, filename);
-  itemname = setItem();
-  originalPrice = setItemCost();
-  discount = setDiscount();
-  //create Item
-  Item item1 = new Item(itemname, originalPrice, discount);
-  finalPrice = item1.getFinalCost();
-  totalDisco = item1.getTotalDiscount();
-  writeToNewFile(user1, day, item1);
-
-/*System.out.println(user1.getName());
-System.out.println(user1.getPassword());
-System.out.println(user1.getFilename());
-System.out.println("Final cost of " + item1.getName() + ": $"+  finalPrice);*/
-}//close main method
-
+  System.out.println("Thanks for using Yang's Money Tracker!");
+  System.out.println("$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$");
+  }//close main
+  public static int fileChoice(){
+   int input;
+   //create a Scanner object to read input
+   Scanner keyboard = new Scanner (System.in);
+   System.out.print("Would you like to (1) create a new file or (2) add to an existing file? ");
+   input = keyboard.nextInt();
+   keyboard.nextLine();
+   return input;
+ }
   public static String createNewFile() throws IOException{
-  String fName;
-  Scanner keyboard = new Scanner (System.in);
-  System.out.print("Enter the filename: ");
-  fName = keyboard.nextLine();
-  File file = new File (fName);
-  while (file.exists()){
+   String fName;
+   Scanner keyboard = new Scanner (System.in);
+   System.out.print("Enter the filename: ");
+   fName = keyboard.nextLine();
+   File file = new File (fName);
+    while (file.exists()){
     System.out.println("The file " + fName + " already exists");
     System.out.print("Enter a new filename: ");
     fName = keyboard.nextLine();
   }
+    return fName;
+}
+ public static String openFile () throws IOException{
+  String fName;
+  Scanner keyboard = new Scanner (System.in);
+  System.out.print("Enter the filename: ");
+  fName = keyboard.nextLine();
+  FileWriter fwriter = new FileWriter(fName, true);
+  PrintWriter outputFile = new PrintWriter(fwriter);
   return fName;
 }
   public static String setUsername (){
-  String uName;
-  Scanner keyboard = new Scanner (System.in);
-  System.out.print("Enter your username: ");
-  uName = keyboard.nextLine();
-  return uName;
+   String uName;
+   Scanner keyboard = new Scanner (System.in);
+   System.out.print("Enter your username: ");
+   uName = keyboard.nextLine();
+   return uName;
   }
   public static String setPassword (){
-  String pWord;
-  Scanner keyboard = new Scanner (System.in);
-  System.out.print("Enter your password: ");
-  pWord = keyboard.nextLine();
-  return pWord;
+   String pWord;
+   Scanner keyboard = new Scanner (System.in);
+   System.out.print("Enter your password: ");
+   pWord = keyboard.nextLine();
+   return pWord;
 }
-public static String getDay (){
-String userDay;
-boolean check = false;
-Scanner keyboard = new Scanner (System.in);
-System.out.print("Enter the day: ");
-userDay = keyboard.nextLine();
-switch (userDay){
-  case "Monday":
-  case "monday":
-  check = true;
-  break;
-  case "Tuesday":
-  case "tuesday":
-  check = true;
-  break;
-  case "Wednesday":
-  case "wednesday":
-  check = true;
-  break;
-  case "Thursday":
-  case "thursday":
-  check = true;
-  break;
-  case "Friday":
-  case "friday":
-  check = true;
-  break;
-  case "Saturday":
-  case "saturday":
-  check = true;
-  break;
-  case "Sunday":
-  case "sunday":
-  check = true;
-  break;
-  default:
-  check = false;
-  }
-  while (!check){
-    System.out.print("Invalid input. Enter day of the week again: ");
-    userDay = keyboard.nextLine();
+ public static String setDay (){
+  String userDay;
+  boolean check = false;
+  Scanner keyboard = new Scanner (System.in);
+  System.out.print("Enter the day: ");
+  userDay = keyboard.nextLine();
+   switch (userDay){
+    case "Monday":
+    case "monday":
     check = true;
+    break;
+    case "Tuesday":
+    case "tuesday":
+    check = true;
+    break;
+    case "Wednesday":
+    case "wednesday":
+    check = true;
+    break;
+    case "Thursday":
+    case "thursday":
+    check = true;
+    break;
+    case "Friday":
+    case "friday":
+    check = true;
+    break;
+    case "Saturday":
+    case "saturday":
+    check = true;
+    break;
+    case "Sunday":
+    case "sunday":
+    check = true;
+    break;
+    default:
+    check = false;
+  }
+    while (!check){
+      System.out.print("Invalid input. Enter day of the week again: ");
+      userDay = keyboard.nextLine();
+      check = true;
 }
 //need to reset flag, ouuuuuuuu???????
-return userDay;
+      return userDay;
 }
-public static String setItem (){
-String userItem;
-Scanner keyboard = new Scanner (System.in);
-System.out.print("Enter the item name: ");
-userItem = keyboard.nextLine();
-return userItem;
+  public static String setItem (){
+   String userItem;
+   Scanner keyboard = new Scanner (System.in);
+   System.out.print("Enter the item name: ");
+   userItem = keyboard.nextLine();
+  return userItem;
 }
 public static double setItemCost (){
-
   double itemCost, check = 0;
   Scanner keyboard = new Scanner (System.in);
   System.out.print("Enter the original item price: ");
@@ -126,7 +164,6 @@ public static double setItemCost (){
   return itemCost;
 }
 public static double setDiscount (){
-
   double promo, check = 0;
   Scanner keyboard = new Scanner (System.in);
   System.out.print("Enter the discount: ");
@@ -138,17 +175,34 @@ public static double setDiscount (){
 }
   return promo;
 }
-/*public static void writeToNewFile(String fName, String uName, String userItem, String day, double originalCost, double disco, double finalCost) throws IOException{
-PrintWriter outputFile = new PrintWriter (fName);
-outputFile.printf("Username: %s, Day: %s, Item: %s\n", uName, day, userItem);
-outputFile.printf("You saved $%,.2f\n", disco);
-outputFile.printf("Your final cost is $%,.2f\n", finalCost);
+public static boolean addData (){
+  Scanner keyboard = new Scanner (System.in);
+  String input;
+  boolean oneMoreTime;
+  char again;
+  System.out.println("Would you like to enter another purchase?");
+  System.out.print("Enter (Y)yes or (N)no: ");
+  input = keyboard.nextLine();
+  again = input.charAt(0);
+  if (again == 'Y' || again == 'y'){
+    oneMoreTime = true;
+  }
+  else {
+    oneMoreTime = false;
+  }
+  return oneMoreTime;
+}
+public static void writeToNewFile(User testUser, String day, Item testItem) throws IOException{
+PrintWriter outputFile = new PrintWriter (testUser.getFilename());
+outputFile.printf("Username: %s, Day: %s, Item: %s\n", testUser.getName(), day, testItem.getName());
+outputFile.printf("You saved $%,.2f\n", testItem.getTotalDiscount());
+outputFile.printf("Your final cost is $%,.2f\n", testItem.getFinalCost());
 //outputFile.printf("Accumulated savings of $%,.2f\n", totalSavings);
 //close the file
 outputFile.close();
 System.out.println("Data written to the file.");
-}*/
-public static void writeToNewFile(User testUser, String day, Item testItem) throws IOException{
+}
+public static void addToFile(User testUser, String day, Item testItem) throws IOException{
 PrintWriter outputFile = new PrintWriter (testUser.getFilename());
 outputFile.printf("Username: %s, Day: %s, Item: %s\n", testUser.getName(), day, testItem.getName());
 outputFile.printf("You saved $%,.2f\n", testItem.getTotalDiscount());
