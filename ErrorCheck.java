5fb6e656e52dc05fe0578a1a83a2bda36c648432
mr.debbie <giyoungchong1980@gmail.com>
2017-02-04 22:04:21 -0600

    Add static methods

--- ErrorCheck.java
+++ ErrorCheck.java
@@ -1,68 +1,12 @@
 import java.util.Scanner;
 //class definition for validating user input
-
+//static methods only
 public class ErrorCheck{
- private String textInput;
- private double decimalInput;
- private int intInput;
 
- public ErrorCheck(){
-  textInput = " ";
-  decimalInput = 0.0;
-  intInput = 0;
+ public static String checkDay(){
+
  }
 
- private void setText(String textTest){
- textInput = textTest;
- }
- private void setDecimal(double decimalTest){
- decimalInput = decimalTest;
- }
- private void setInt(int intTest){
- intInput = intTest;
- }
- private String checkDay(String textTest){
- boolean check = false;
- Scanner keyboard =  new Scanner (System.in);
- switch (textTest){
-  case "Monday":
-  case "monday":
-  check = true;
-  break;
-  case "Tuesday":
-  case "tuesday":
-  check = true;
-  break;
-  case "Wednesday":
-  case "wednesday":
-  check = true;
-  break;
-  case "Thursday":
-  case "thursday":
-  check = true;
-  break;
-  case "Friday":
-  case "friday":
-  check = true;
-  break;
-  case "Saturday":
-  case "saturday":
-  check = true;
-  break;
-  case "Sunday":
-  case "sunday":
-  check = true;
-  break;
-  default:
-  check = false;
-}
-  while (!check){
-    System.out.print("Invalid input. Enter day of the week again: ");
-    userDay = keyboard.nextLine();
-    check = true;
-}
-//need to reset flag, ouuuuuuuu???????
-    return textTest;
- }
+
 
 }
